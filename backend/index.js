94192e3d72566da3c5e6f868c2b3c0805fe3a0fe
Erol Mehmed <erol.mehmed07@gmail.com>
2024-04-28 20:52:59 +0300

    notes stream: frontend progress and backend beginning

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,19 @@
+require('dotenv').config();
+
 const express  = require('express');
+const cors = require('cors');
 
-const { PORT } = require('./constants');
+const PORT = process.env.PORT;
+const routes = require('./routes');
+const sequelize = require('./config/databaseConfig');
 
+const app = express();
+
+app.use(routes);
+app.use(cors());
+
+sequelize.authenticate()
+.then(() => console.log('Database connected...'))
+.catch(err => console.log('Error: ' + err));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
--- frontend/src/features/MainContent/MainContent.tsx
+++ frontend/src/features/MainContent/MainContent.tsx
@@ -1,14 +1,16 @@
-import { Box, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import "./MainContent.scss";
 import {useEffect, useRef, useState} from "react";
 
 const MainContent = () => {
   const [ openEditor, setOpenEditor ] = useState(false);
-  const editorRef = useRef(null);
+  const editorRef = useRef<HTMLDivElement | null>(null);
+  const formRef = useRef<HTMLFormElement | null>(null);
   
   const handleClickOutside = (event: any) => {
       if (editorRef.current && !editorRef.current.contains(event.target) && openEditor) {
-        setOpenEditor(false);
+        const closeButton: HTMLButtonElement | null = editorRef.current?.querySelector('button[type="submit"]');
+        closeButton?.click();
       }
     };
   
@@ -19,19 +21,42 @@ const MainContent = () => {
       document.removeEventListener('click', handleClickOutside);
     }
   });
+
+  useEffect(() => {
+    if (openEditor) {
+      const inputElement = editorRef.current?.querySelector('input[name="note"]');
+      
+      if (inputElement instanceof HTMLElement) {
+        inputElement.focus();
+      }
+    }
+  }, [openEditor]);
   
   return (
     <Grid container className="main-content" justifyContent="center">
-      <Grid item xs={6} className="editor" ref={editorRef}>
+      <Grid item xs={6} className="editor" ref={editorRef} onClick={(event) => {
+        event.stopPropagation();
+        setOpenEditor(true)}}>
         {
         openEditor ?
-          (<Box className="opened" display="flex" flexDirection="column">
-            <input name="title" placeholder="Title"></input>
-            <input name="note" placeholder="Take a note..."></input>
-          </Box>) :
-          (<Box className="closed" onClick={() => setOpenEditor(true)}>
-            Take a note...
-          </Box>)
+          (<form className="opened" ref={formRef} onSubmit={(event) => {
+            event.preventDefault();
+            event.stopPropagation();
+          }}>
+              <input name="title" placeholder="Title"></input>
+              <input name="note" placeholder="Take a note..."></input>
+              <button type="submit" onClick={(event) => {
+                event.stopPropagation();
+                setTimeout(() => {
+                  setOpenEditor(false);
+                }, 0);
+              }}>
+                Close
+              </button>
+            </form>) :
+          (<p className="closed">
+              Take a note...
+          </p>)
         }
       </Grid>
     </Grid>
