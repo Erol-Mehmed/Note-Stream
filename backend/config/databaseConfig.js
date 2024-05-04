42a94b57d9cc42ce3f74700cade6a377799c4a43
Erol Mehmed <erol.mehmed07@gmail.com>
2024-05-04 09:51:00 +0300

    note stream: sql queries logging removed

--- backend/config/databaseConfig.js
+++ backend/config/databaseConfig.js
@@ -5,7 +5,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'postgres',
-  logging: console.log,
+  logging: false,
 });
 
 sequelize.sync({ alter: true })
