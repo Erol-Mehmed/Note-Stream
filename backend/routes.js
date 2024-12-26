6fdcb3fe96e105045a9a9d75551caf2db9e7695e
Erol Mehmed <erol.mehmed07@gmail.com>
2024-12-26 21:48:42 +0200

    backend refactor

--- backend/routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const notesController = require('./controllers/notesController');
-const authController = require('./controllers/authController');
-
-router.use('/notes', notesController);
-router.use('/auth', authController);
-
-module.exports = router;
