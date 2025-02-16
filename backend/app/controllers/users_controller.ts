faa2dde87366a0f353e10ad9e7d6d4da0c6c2706
Erol Mehmed <erol.mehmed07@gmail.com>
2025-02-17 00:08:24 +0200

    backend spring boot refactor

--- backend/app/controllers/users_controller.ts
+++
@@ -1,9 +0,0 @@
-import { HttpContext } from '@adonisjs/core/http';
-import User from '#models/user';
-
-export default class UsersController {
-  async index({ response }: HttpContext) {
-    const users = await User.all();
-    return response.json(users);
-  }
-}
